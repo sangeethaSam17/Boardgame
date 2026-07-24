f060899b816dddbe7ae65d7390086d902588afab
sangeethaSam17 <sam.sangeetha17@gmail.com>
2026-07-24 11:13:37 +0530

    modified ec.tf from terraform

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "eks-manager" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
   iam_instance_profile = aws_iam_instance_profile.test_profile.name
-
+  key_name = aws_key_pair.eks-manager.key_name
   tags = {
     Name = var.instance_name
   }
@@ -28,3 +28,7 @@ resource "aws_iam_instance_profile" "test_profile" {
   name = "eks-role"
   role = aws_iam_role.instance.name
 }
+
+resource "aws_key_pair" "eks-manager" {
+  key_name   = var.key_name
+  public_key = file(${path.module}/keys/eks-manager.pub)
