10c65df57dafc99d9114f205217a42dad78f1ffa
sangeethaSam17 <sam.sangeetha17@gmail.com>
2026-07-25 06:07:31 +0530

    Updated output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,7 +1,7 @@
 output "public_ip" {
-  value = aws_instance.eks_management_node.public_ip
+  value = aws_instance.eks-manager.public_ip
 }
 
 output "public_dns" {
-  value = aws_instance.eks_management_node.public_dns
+  value = aws_instance.eks-manager.public_dns
 }
